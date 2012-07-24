7a7f3a001a0d8f9f9860dcb4b049341c00845372
Chao Zhang <zhang.chao.eric@gmail.com>
2012-07-24 21:29:12 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # get "pages/home"
   root to: 'pages#home'
@@ -14,6 +15,8 @@ SampleApp::Application.routes.draw do
   match '/help', to: 'pages#help'
 
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
